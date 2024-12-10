11ca893ea854f308c2425a25fd5dd2b6f853cbbf
Bstrinic <BogdanStrinic003@gmail.com>
2024-12-09 22:56:51 -0700

    attempted to make navbar

---
+++ app/component/Navbar.js
@@ -0,0 +1,38 @@
+
+// import { useRouter } from "next/navigation";
+// import Link from 'next/link';
+// import Image from 'next/image';
+
+// export default function Navbar() {
+
+//   return (
+//     <div className="top-0 fixed">
+//       <div>
+//         {/* Application Name */}
+//         <h1>FilmRewind</h1>
+//         {/* Image */}
+//         <Image
+//           src="/logo.png"
+//           alt="Application Logo"
+//           width={40}
+//           height={40}
+//           className="w-10 h-10 mb-5"
+//         />
+//       </div>
+//       {/* Home Button, Profile Button, Signout Button */}
+//       <div>
+//         <ul>
+//           <li>
+//             <Link href="/home">Home</Link>
+//           </li>
+//           <li>
+//             <Link href="/profile">Profile</Link>
+//           </li>
+//           <li>
+//             <Link href="/logout">Logout</Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
