aff6b94b35091cdb7e6a2ce46fd8922c9bb453c4
Bstrinic <BogdanStrinic003@gmail.com>
2025-01-01 23:43:02 -0700

    updated file structure

--- app/pages/HomePage.js
+++ app/pages/HomePage.js
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import MovieBox from "../movie/page";
+import MovieBox from "../component/MoviePopUp";
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
--- app/pages/page.js
+++ app/pages/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import SignIn from "./signin/page";
+import SignIn from "./SigninPage";
 
 export default function Home() {
   return (
